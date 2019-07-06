61239897a62ed8c1cc02855db4f8a80af1db0176
Manuel Martin <manuel.martintorres@yahoo.es>
2019-07-06 11:24:01 +0200

    Styling explanation

--- 01_Multibrowser.py
+++ 01_Multibrowser.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+# Chrome driver
 driver=webdriver.Chrome(executable_path="C:/DRIVERS/Selenium_drivers/chromedriver_win32/chromedriver.exe")
+
+# Firefox driver
 # driver=webdriver.Firefox(executable_path="C:/DRIVERS/Selenium_drivers/geckodriver-v0.24.0-win64/geckodriver.exe")
 
 driver.get("http://newtours.demoaut.com/")
@@ -15,6 +18,6 @@ print(driver.current_url)
 # returns the HTML code for the page
 print(driver.page_source)
 
-# close the browser
+# close the browser (only focussed browser)
 driver.close()
 
