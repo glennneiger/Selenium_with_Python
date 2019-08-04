6c4fd3917cb10667d89712fc343f7050bb413ccf
Manuel Martin <manuel.martintorres@yahoo.es>
2019-08-04 06:32:43 +0200

    Explanation for chrome options

--- 21_Download_files_chrome.py
+++ 21_Download_files_chrome.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+# This is in case you want to save it somewhere else than the default location
 chrome_options = Options()
 chrome_options.add_experimental_option("prefs", {"download.default_directory": r"C:\Users\Usuario\Downloads"})
 
