6b1ed336fae882fd5f2cb636c3e0a8da4a715c95
Manuel Martin <manuel.martintorres@yahoo.es>
2019-08-04 06:53:04 +0200

    reordering code

--- 21_Download_files_chrome.py
+++ 21_Download_files_chrome.py
@@ -2,14 +2,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+driver = webdriver.Chrome(executable_path="C:/DRIVERS/Selenium_drivers/chromedriver_win32/chromedriver.exe",
+                          options=chrome_options)
+driver.get("http://demo.automationtesting.in/FileDownload.html")
+
 # This is in case you want to save it somewhere else than the default location
 chrome_options = Options()
 chrome_options.add_experimental_option("prefs", {"download.default_directory": r"C:\Users\Usuario\Downloads"})
 
-driver = webdriver.Chrome(executable_path="C:/DRIVERS/Selenium_drivers/chromedriver_win32/chromedriver.exe",
-                          options=chrome_options)
-
-driver.get("http://demo.automationtesting.in/FileDownload.html")
 driver.maximize_window()
 
 driver.find_element_by_id("textbox").send_keys("testing download text file")
