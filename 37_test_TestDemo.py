30520cbab3dae10c2356fe389c059c96ad62e06c
Manuel Martin <manuel.martintorres@yahoo.es>
2019-08-22 01:35:29 +0200

    Basics of Pytest (how to exectute it)

---
+++ 37_test_TestDemo.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+def testMethod1():
+    print("This is Test method1")
+
+
+def testMethod2():
+    print("This is Test method2")
+
+# pytest (executes basic test)
+# pytest -v -s (also returns details and print statements)
